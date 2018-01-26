2cf9c21ebc5996f4aadf6694a32d8cce7af2cc7c
damys <damys@foxmail.com>
2018-01-26 09:26:36 +0800

    add app.vue

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -3,38 +3,53 @@
 <!--     <img src="./assets/logo.png"> -->
     <header-vue></header-vue>
     <for-vue></for-vue>
-    <sub-vue text1='text1-111' v-bind:text2='text2'></sub-vue>
+    <sub-vue text1='text1-111' v-bind:text2='text2' @sayHello="listen"></sub-vue>
+
+    <ui-vue></ui-vue>
     <footer-vue></footer-vue>
     <component-app></component-app>
+
 
   </div>
 </template>
 
 <script>
-// 引用组件
+//-- 引用组件 -------------------------
+var componentApp = {  template: `<p>我是组件A</p>` }
 import headerVue from './components/header.vue'
 import forVue from './components/for.vue'
 import subVue from './components/sub.vue'
 import footerVue from './components/footer.vue'
-var componentApp = {  template: `<p>我是组件A</p>` }
-
+import uiVue from './components/ui.vue'
 
 export default {
-  name: 'App',
   data(){
     return {
       text2:'v-bind text2-222'
     }
   },
 
+  methods:{
+    //-- 组件传值：子传父 ----------------------
+    // ES6 写法
+    listen(...data){
+        console.log(data)
+        console.log(data[0].name)
+    }
+
+    // listen:function(data){
+    //     console.log(data)
+    // }
+  },
 
   // 局部：模板中使用组件
   components: {
+    'componentApp':componentApp,
     'headerVue':headerVue,
     'forVue':forVue,
     'subVue':subVue,
-    'footerVue':footerVue,
-    'componentApp':componentApp,
+    'footerVue':footerVue,    
+    'uiVue':uiVue
   }
 
 }
